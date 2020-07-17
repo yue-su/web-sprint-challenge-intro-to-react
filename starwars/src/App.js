8da7ce99cea4650b1d23f9d6d237f0661854bc0e
Yue Su <yuesu@Mac-Pro.local>
2020-07-17 14:55:21 -0300

    adding animations

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,13 +8,6 @@ import Footer from "./components/footer"
 const apiKey = "https://rickandmortyapi.com/api/character/"
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   const [characters, setCharaters] = useState([])
 
   useEffect(() => {
